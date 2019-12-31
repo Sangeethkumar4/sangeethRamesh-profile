40a9390c89974427a13468a7f3e85344c8fd335b
Sangeeth Kumar Ramesh <SangeethKumar.Ramesh@mrcooper.com>
2019-12-31 12:32:23 +0530

    Added github and linked in links

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,7 @@ import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import {useStyles} from './personalInfo';
 import { makeStyles } from '@material-ui/core/styles';
 
-let array = ['9080523535', 'kumarsangeeth4@gmail.com', 'Github', 'LinkedIn'];
+let array = [{key: '9080523535'}, {key: 'kumarsangeeth4@gmail.com'},{key: 'Github', link: 'https://github.com/Sangeethkumar4'} , {key: 'LinkedIn', link: 'https://www.linkedin.com/in/sangeeth-kumar-b099829a/'}];
 
 const createStyles = makeStyles(theme => ({
     card: {
@@ -19,6 +19,9 @@ const createStyles = makeStyles(theme => ({
         paddingTop: 5,
         fontSize : 20,
         paddingLeft: 10
+    },
+    link: {
+        color: "white"
     }
  }));
 
@@ -31,12 +34,15 @@ export function Contact() {
                 <div className={classes.icons}>
                     <div className={classes.icon, styles.iconOverride}><PhoneAndroidRoundedIcon /></div>
                     <div className={classes.icon, styles.iconOverride}><EmailRounded /></div>
-                    <div className={classes.icon, styles.iconOverride}><a href = "https://github.com/Sangeethkumar4"></a><GitHubIcon /></div>
-                    <div className={classes.icon, styles.iconOverride}><a href = "https://www.linkedin.com/in/sangeeth-kumar-b099829a/"></a><LinkedInIcon /></div>
+                    <div className={classes.icon, styles.iconOverride}><a href = "https://github.com/Sangeethkumar4" className = {styles.link}  target="_blank"> <GitHubIcon /></a></div>
+                    <div className={classes.icon, styles.iconOverride}><a href = "https://www.linkedin.com/in/sangeeth-kumar-b099829a/"  target="_blank" className = {styles.link}><LinkedInIcon /></a></div>
                 </div>
                 <div className={classes.textContainer}>
-                    {array.map(function (name, index) {
-                        return <div key={index} className={classes.text, styles.textStyleOverride}>{name}</div>;
+                    {array.map(function (each, index) {
+                        if(each.link != undefined)
+                        return <a href= {each.link} key={index} className = {styles.link} target="_blank"><div className={classes.text, styles.textStyleOverride}>{each.key}</div></a>;
+                        else
+                            return (<div className={classes.text, styles.textStyleOverride} key={index}>{each.key}</div>);
                     })}
                 </div>
         </div>
